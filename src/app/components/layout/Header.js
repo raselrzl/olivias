70df10de183129f833ea88ebb57ed5ab820cd572
Rasel Miah <rasel6041@gmail.com>
2024-07-06 14:22:08 +0100

    Update ESLint configuration

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -1,20 +1,24 @@
 "use client";
 import { useState } from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
 import Image from 'next/image';
 import { GiHamburgerMenu } from "react-icons/gi";
+import { usePathname } from 'next/navigation';
+
 export default function Header() {
   const [isNavVisible, setIsNavVisible] = useState(false);
-
+  const pathname = usePathname();
   const toggleNav = () => {
     setIsNavVisible(!isNavVisible);
   };
+
+  const isActive = (path) => pathname === path;
+
   return (
     <header className="flex px-2 py-6 items-center justify-between">
       <Link href="/">
         <Image src="/logo.png" width={40} height={40} alt='JAYS' />
       </Link>
-     {/*  <Link href={'/'} className="font-black text-amber-200 text-xl ">Welcome to JAY'S</Link> */}
       <div className="relative">
         <div className="relative">
           <button onClick={toggleNav} className="block sm:hidden">
@@ -34,17 +38,17 @@ export default function Header() {
             <button onClick={toggleNav} className="self-end mb-4 text-xl">
               ✕
             </button>
-            <Link href={'/'} className="text-amber-200">Home</Link>
-            <Link href={'/menu'} className="text-amber-200">Menu</Link>
-            <Link href={'/about'} className="text-amber-200">About</Link>
-            <Link href={'/contact'} className="text-amber-200">Contact</Link>
+            <Link href={'/'} className={`text-amber-200 ${isActive('/') ? 'font-bold' : ''}`}>Home</Link>
+            <Link href={'/menu'} className={`text-amber-200 ${isActive('/menu') ? 'font-bold' : ''}`}>Menu</Link>
+            <Link href={'/about'} className={`text-amber-200 ${isActive('/about') ? 'font-bold' : ''}`}>About</Link>
+            <Link href={'/contact'} className={`text-amber-200 ${isActive('/contact') ? 'font-bold' : ''}`}>Contact</Link>
           </nav>
 
           <nav className="hidden sm:flex items-center gap-6 text-gray-600 font-semibold">
-            <Link href={'/'} className="text-amber-200">Home</Link>
-            <Link href={'/menu'} className="text-amber-200">Menu</Link>
-            <Link href={'/about'} className="text-amber-200">About</Link>
-            <Link href={'/contact'} className="text-amber-200">Contact</Link>
+            <Link href={'/'} className={`text-amber-200 ${isActive('/') ? 'font-bold' : ''}`}>Home</Link>
+            <Link href={'/menu'} className={`text-amber-200 ${isActive('/menu') ? 'font-bold' : ''}`}>Menu</Link>
+            <Link href={'/about'} className={`text-amber-200 ${isActive('/about') ? 'font-bold' : ''}`}>About</Link>
+            <Link href={'/contact'} className={`text-amber-200 ${isActive('/contact') ? 'font-bold' : ''}`}>Contact</Link>
           </nav>
         </div>
       </div>
