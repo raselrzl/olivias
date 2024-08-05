cb31b2406e8af1d795e701da227a3dba1d69ea4c
Rasel Miah <rasel6041@gmail.com>
2024-08-05 23:41:18 +0100

    update notification button

--- src/app/components/notification.js
+++ src/app/components/notification.js
@@ -6,38 +6,40 @@ export default function Notification() {
   const [latestNotification, setLatestNotification] = useState(null);
   const [error, setError] = useState(null);
 
-  const fetchLatestNotification = async () => {
-    try {
-      const response = await fetch(`${BASE_API_URL}/api/notification/latest`);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      setLatestNotification(data.message || 'No notifications available');
-    } catch (error) {
-      console.error('Error fetching latest notification:', error);
-      setError('Error fetching latest notification: ' + error.message);
-    }
-  };
-
-  // Fetch data on component mount and set up interval
   useEffect(() => {
-    fetchLatestNotification(); // Fetch data on component mount
+    // Function to fetch the latest notification
+    const fetchLatestNotification = async () => {
+      try {
+        const response = await fetch(`${BASE_API_URL}/api/notification/latest`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setLatestNotification(data.message || 'No notifications available');
+      } catch (error) {
+        console.error('Error fetching latest notification:', error);
+        setError('Error fetching latest notification: ' + error.message);
+      }
+    };
 
+    // Fetch the latest notification immediately on component mount
+    fetchLatestNotification();
+
+    // Set up interval to fetch data every 1 minute
     const intervalId = setInterval(() => {
-      fetchLatestNotification(); // Fetch data every 1 minute
-    }, 2 * 60 * 1000); // 1 minute in milliseconds
+      fetchLatestNotification();
+    }, 1 * 60 * 1000); // 1 minute in milliseconds
 
     // Clean up the interval on component unmount
     return () => clearInterval(intervalId);
-  }, []);
+  }, []); // Empty dependency array ensures this runs only on mount and unmount
 
   if (error) {
     return <div className='text-red-500 text-center'>Error: {error}</div>;
   }
 
   return (
-    <div>     
+    <div>
       <div>
         <span className="text-xxs">{latestNotification || 'JAYS'}</span>
       </div>
