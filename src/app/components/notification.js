f21cff9f00f63c6cbee55704a812e1bd84495ec8
Rasel Miah <rasel6041@gmail.com>
2024-08-05 22:44:43 +0100

    time interval for notification

--- src/app/components/notification.js
+++ src/app/components/notification.js
@@ -1,26 +1,21 @@
 "use client";
 import { useState, useEffect } from 'react';
-import LoadingSpinner from './loading-spinner';
-import { BASE_API_URL } from '@/lib/utils';
 
 export default function Notification() {
   const [latestNotification, setLatestNotification] = useState(null);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true); // Initial loading state should be true
 
   const fetchLatestNotification = async () => {
     try {
-      const response = await fetch(`${BASE_API_URL}/api/notification/latest`);
+      const response = await fetch('/api/notification/latest');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      setLatestNotification(data.message);
+      setLatestNotification(data.message || 'No notifications available');
     } catch (error) {
       console.error('Error fetching latest notification:', error);
       setError('Error fetching latest notification: ' + error.message);
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -29,26 +24,18 @@ export default function Notification() {
 
     const intervalId = setInterval(() => {
       fetchLatestNotification(); // Fetch data every 2 minutes
-    }, 2 * 60 * 1000); // 2 minutes in milliseconds
+    }, 1 * 60 * 1000); // 2 minutes in milliseconds
 
     return () => clearInterval(intervalId); // Clear interval on component unmount
   }, []);
-
-  if (loading) {
-    return <div><LoadingSpinner /></div>;
-  }
 
   if (error) {
     return <div className='text-red-500 text-center'>Error: {error}</div>;
   }
 
   return (
-    <div className="text-amber-100 whitespace-nowrap animate-marquee">
-      {latestNotification ? (
+    <div className="text-amber-100 whitespace-nowrap animate-marquee"> 
         <span className="text-xxs">{latestNotification}</span>
-      ) : (
-        <span className="text-xxs">No notifications available</span>
-      )}
     </div>
   );
 }
