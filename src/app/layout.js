b3383065f7b88e4a9b479cc53ee6c62bbf9679f6
Rasel Miah <rasel6041@gmail.com>
2024-11-24 09:27:06 +0100

    Fix missing dependencies

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 import Header from "./components/Header";
 import { Footer } from "./components/Footer";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // Adjust content paths based on your project structure
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}', // You might not need this if your pages are inside 'src'
+    './components/**/*.{js,ts,jsx,tsx}', // Same for this if inside 'src'
   ],
   theme: {
     extend: {
-      colors:{
-        primary:'#B0AE38'
+      colors: {
+        primary: '#B0AE38',
       },
       fontSize: {
         'xxs': '0.625rem', // equivalent to 10px
